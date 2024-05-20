0eb67a4669f10305f7f781c2644df31979bd9665
Aparicio Junior <apariciojunior11@gmail.com>
2024-05-20 16:14:52 -0300

    Aplicação finalizada CRUD Express JS com MongoDB integrado

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { MongoClient, ObjectId, Collection } = require('mongodb');
 
 // Conexão com o banco de dados
-const uri = 'mongodb://localhost:27017';
+const uri = 'mongodb+srv://admin:QkqieN7nAyuzGv41QVHRFbGXy@cluster0.qq6hgi9.mongodb.net';
 const dbName = 'crud-expressjs-mongodb';
 
 // Função para conectar ao banco de dados
